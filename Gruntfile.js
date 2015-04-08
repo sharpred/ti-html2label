64b360e403d68091054fac6877c67c6f725f632c
Paul Ryan <paul.ryan@stepupsoftware.co.uk>
2015-04-08 09:23:42 +0100

    added sdk target to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,52 +2,59 @@
 
 module.exports = function(grunt) {
 
-  grunt.initConfig({
+    grunt.initConfig({
 
-    pkg: grunt.file.readJSON('package.json'),
+        pkg : grunt.file.readJSON('package.json'),
 
-    clean: {
-      unzip: ['modules'],
-      modules: ['example/modules'],
-      app: ['example/build']
-    },
+        clean : {
+            unzip : ['modules'],
+            modules : ['example/modules'],
+            app : ['example/build']
+        },
 
-    titaniumifier: {
-      module: {
-        src: '.',
-        dest: '.'
-      }
-    },
+        titaniumifier : {
+            module : {
+                src : '.',
+                dest : '.'
+            }
+        },
 
-    titanium: {
-      ios: {
-        options: {
-          command: 'build',
-          logLevel: 'debug',
-          projectDir: './example',
-          platform: 'ios'
+        titanium : {
+            ios : {
+                options : {
+                    command : 'build',
+                    logLevel : 'debug',
+                    projectDir : './example',
+                    platform : 'ios'
+                }
+            },
+            sdk : {
+                options : {
+                    command : 'sdk',
+                    args : ['select', '3.5.0.GA']
+                }
+            }
+        },
+
+        unzip : {
+            module : {
+                src : '<%= pkg.titaniumManifest.moduleid %>-commonjs-<%= pkg.version %>.zip',
+                dest : 'example'
+            }
         }
-      }
-    },
 
-    unzip: {
-      module: {
-        src: '<%= pkg.titaniumManifest.moduleid %>-commonjs-<%= pkg.version %>.zip',
-        dest: 'example'
-      }
-    }
+    });
 
-  });
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-titaniumifier');
+    grunt.loadNpmTasks('grunt-titanium');
+    grunt.loadNpmTasks('grunt-zip');
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-titaniumifier');
-  grunt.loadNpmTasks('grunt-titanium');
-  grunt.loadNpmTasks('grunt-zip');
+    grunt.registerTask('build', ['titaniumifier:module']);
 
-  grunt.registerTask('build', ['titaniumifier:module']);
-  grunt.registerTask('test', ['unzip:module', 'titanium:ios', 'clean:unzip']);
+    grunt.registerTask('test', ['unzip:module', 'titanium:sdk','titanium:ios', 'clean:unzip']);
 
-  grunt.registerTask('ios', ['clean', 'build', 'test']);
+    grunt.registerTask('ios', ['clean', 'build', 'test']);
 
-  grunt.registerTask('default', ['ios']);
+    grunt.registerTask('default', ['ios']);
 };
