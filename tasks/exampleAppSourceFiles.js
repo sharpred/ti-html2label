6f664715e3e1248d9adb1a45da800d094a8dc57c
Paul Ryan <paul.ryan@stepupsoftware.co.uk>
2017-08-08 17:51:22 +0100

    update file copy

--- tasks/exampleAppSourceFiles.js
+++ tasks/exampleAppSourceFiles.js
@@ -2,17 +2,22 @@ module.exports = function (grunt) {
     grunt.registerTask('copyexamplefiles', 'copies required source to the newly created example app', function () {
         var srcFolder = process.cwd() + "/exampleSource/";
         var destFolder = process.cwd() + "/example/";
-        [{
+        var toCopy = [{
             srcpath: srcFolder + "index.js",
             destpath: destFolder + "app/controllers/index.js"
         }, {
             srcpath: srcFolder + "index.tss",
-            destpath: "app/styles/index.tss"
+            destpath: destFolder + "app/styles/index.tss"
         }, {
             srcpath: srcFolder + "index.xml",
-            destpath: "app/views/index.xml"
-        }].forEach(function (file) {
+            destpath: destFolder + "app/views/index.xml"
+        }];
+
+        toCopy.forEach(function (file) {
+            grunt.log.ok(file.srcpath);
+            grunt.log.ok(file.destpath);
             grunt.file.copy(file.srcpath, file.destpath);
         });
+
     });
 }
