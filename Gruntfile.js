3647fec0ace7413f625df231d011bff5bc4acf5a
Paul Ryan <paul.ryan@stepupsoftware.co.uk>
2017-08-08 17:34:35 +0100

    replaced ti deps with appc

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,67 @@
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     grunt.initConfig({
 
-        pkg : grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON('package.json'),
 
-        clean : {
-            unzip : ['modules'],
-            modules : ['example/modules'],
-            app : ['example/build']
+        clean: {
+            unzip: ['modules'],
+            modules: ['example/modules'],
+            app: ['example/build']
         },
 
-        titaniumifier : {
-            module : {
-                src : '.',
-                dest : '.'
+        titaniumifier: {
+            module: {
+                src: '.',
+                dest: '.'
             }
         },
-
-        titanium : {
-            android : {
-                options : {
-                    command : 'build',
-                    logLevel : 'debug',
-                    projectDir : './example',
-                    platform : 'android'
-                }
+        "appc-cli": {
+            options: {},
+            new: {
+                command: 'new',
+                args: ['-f', '--no-services', '-t', 'titanium', '--project-dir', './example', '-i', 'com.stepupapps.html2label.example', '-n', 'com.stepupapps.html2label.example']
             },
-            ios : {
-                options : {
-                    command : 'build',
-                    logLevel : 'debug',
-                    projectDir : './example',
-                    platform : 'ios'
-                }
+            whoami: {
+                command: 'whoami'
             },
-            sdk : {
-                options : {
-                    command : 'sdk',
-                    args : ['select', '<%= pkg.titaniumManifest.tisdk %>']
-                }
+            simulator: {
+                command: "run",
+                args: ["-p", "ios", "-C", "60FEF0FE-32B5-44D2-B0AA-038E146AE764", '--project-dir', './example']
+            },
+            sdk: {
+                command: "ti",
+                subcommand: "sdk",
+                args: ["select", '<%= pkg.titaniumManifest.tisdk %>']
+            },
+            ios: {
+                command: "ti",
+                subcommand: "build",
+                options: {
+                    "log-level": "info",
+                    platform: "ios",
+                    "project-dir": "./example",
+                    target: "simulator"
+                },
+                args: ["--noisy"]
+            },
+            android: {
+                command: "ti",
+                subcommand: "build",
+                options: {
+                    "log-level": "info",
+                    platform: "android",
+                    "project-dir": "./example"
+                },
+                args: ["--noisy", "--build-only"]
             }
         },
-
-        unzip : {
-            module : {
-                src : '<%= pkg.titaniumManifest.moduleid %>-commonjs-<%= pkg.version %>.zip',
-                dest : 'example'
+        unzip: {
+            module: {
+                src: '<%= pkg.titaniumManifest.moduleid %>-commonjs-<%= pkg.version %>.zip',
+                dest: 'example'
             }
         }
 
@@ -55,18 +69,20 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-titaniumifier');
-    grunt.loadNpmTasks('grunt-titanium');
+    grunt.loadNpmTasks('grunt-appc-cli');
     grunt.loadNpmTasks('grunt-zip');
     grunt.loadTasks('tasks');
     grunt.registerTask('build', ['titaniumifier:module']);
 
-    grunt.registerTask('testios', ['unzip:module', 'titanium:sdk', 'titanium:ios', 'clean:unzip']);
+    grunt.registerTask('testios', ['unzip:module', 'appc-cli:sdk', 'appc-cli:ios', 'clean:unzip']);
 
-    grunt.registerTask('testandroid', ['unzip:module', 'titanium:sdk', 'titanium:android', 'clean:unzip']);
+    grunt.registerTask('testandroid', ['unzip:module', 'appc-cli:sdk', 'appc-cli:android', 'clean:unzip']);
 
-    grunt.registerTask('ios', ['tiapp', 'clean', 'build', 'testios']);
+    grunt.registerTask('ios', ['appc-cli:new', 'tiapp', 'clean', 'build', 'testios']);
 
-    grunt.registerTask('android', ['tiapp', 'clean', 'build', 'testandroid']);
+    grunt.registerTask('android', ['appc-cli:new', 'tiapp', 'clean', 'build', 'testandroid']);
 
     grunt.registerTask('default', ['ios']);
+
+    grunt.registerTask('appc', ['appc-cli:new']);
 };
